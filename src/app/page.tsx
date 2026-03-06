9f0e1d444859e989a6559e93e8a346e27b7e386b
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-03-06 09:49:34 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Stats from "@/components/Statistics";
 import Extro from "@/components/HeroExtro";
 import Gallery from "@/components/Gallery/page";
 import Testimonials from "@/components/Testimonials";
+import Cards from "@/components/OurServicesSection";
 
 export default function HomePage() {
   return (
     <main className="relative mx-auto min-h-screen mt-7 ">
       <Hero />
+      <Cards />
       <About />
       <Stats />
       <Extro />
