9490695e5e61b474cad79d2dc98e4e05866afb00
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-03-11 14:21:49 +0300

    t

--- src/app/exercise-therapy/page.tsx
+++ src/app/exercise-therapy/page.tsx
@@ -16,7 +16,7 @@ import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 const carouselImages = [
   "/images/education and training 2.jpg",
-  "/images/exo2.png",
+  "/images/challenge94.png",
   "/images/exo3.png",
 ];
 
