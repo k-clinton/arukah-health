d7c38b0aa01932a2c966fe09426f0a992abb19b2
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-03-16 15:32:53 +0300

    Optimize image

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
   reactStrictMode: true,
   trailingSlash: true,
   turbopack: {
     root: __dirname,
   },
   images: {
-    unoptimized: true,          // ← add this
+    unoptimized: true, // ← add this
     // remotePatterns: [...],   // if you have any
   },
 };
